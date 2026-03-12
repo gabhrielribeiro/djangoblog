a0444d3f70dee549d817fa4074df7b7bc6438151
gabhrielribeiro <gabhrielribeiro@gmail.com>
2026-03-12 20:48:09 -0300

    admin

--- core/settings.py
+++ core/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'usuarios'
+    'usuarios',
+    'usuarios.apps.UsuariosConfig'
 ]
 
 MIDDLEWARE = [
--- usuarios/apps.py
+++ usuarios/apps.py
@@ -3,3 +3,23 @@ from django.apps import AppConfig
 
 class UsuariosConfig(AppConfig):
     name = 'usuarios'
+
+    def ready(self):
+        try:
+            import os
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+
+            email = os.getenv("EMAIL_ADMIN")
+            senha = os.getenv("SENHA_ADMIN")
+
+            if email and senha:
+                if not User.objects.filter(email=email).exists():
+                    User.objects.create_superuser(
+                        username="admin",
+                        email=email,
+                        password=senha
+                    )
+        except Exception:
+            pass

bfef7a9c9822668629558b0c1b471270a0a13d24
gabhrielribeiro <gabhrielribeiro@gmail.com>
2026-03-12 19:20:54 -0300

    primeiro commit

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +77,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
-import dj_database_url
+
 
 DATABASES = {
     "default": dj_database_url.config(
